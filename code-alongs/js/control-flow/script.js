1545a11c190d9f6cb27c2350e4174f6a68a27662
Martyna Król <martyna.krool@gmail.com>
2024-11-27 12:10:44 +1100

    switch challenge

--- code-alongs/js/control-flow/script.js
+++ code-alongs/js/control-flow/script.js
@@ -180,3 +180,231 @@ switch (true) {
     case num > 0 && num < 2:
         console.log("correct number from switch");
 }
+
+// DAYS OF THE WEEK
+
+let day = 14;
+
+switch (day % 7) {
+    case 1:
+        console.log("Today is Monday");
+        break;
+    case 2:
+        console.log("Today is Tuesday");
+        break;
+    case 3:
+        console.log("Today is Wednesday");
+        break;
+    case 4:
+        console.log("Today is Thursday");
+        break;
+    case 5:
+        console.log("Today is Friday");
+        break;
+    case 6:
+        console.log("Today is Saturday");
+        break;
+    case 0:
+        console.log("Today is Sunday");
+        break;
+}
+
+switch (day) {
+    case 1:
+        console.log("Today is Monday");
+        break;
+    case 2:
+        console.log("Today is Tuesday");
+        break;
+    case 3:
+        console.log("Today is Wednesday");
+        break;
+    case 4:
+        console.log("Today is Thursday");
+        break;
+    case 5:
+        console.log("Today is Friday");
+        break;
+    case 6:
+        console.log("Today is Saturday");
+        break;
+    case 7:
+        console.log("Today is Sunday");
+        break;
+    default:
+        console.log("Please enter a number between 1 and 7");
+}
+
+// EYE COLOUR
+// let parent1 = "brown";
+// let parent2 = "green";
+// let eyeInherit = parent1 + parent2;
+
+// let browPercent = 0;
+// let bluePercent = 0;
+// let greenPercent = 0;
+
+// switch (eyeInherit) {
+//     case "BrownBrown":
+//     case "brownbrown":
+//         browPercent = "75%";
+//         greenPercent = "18.75%";
+//         bluePercent = "6.25%";
+//         // console.log(
+//         //     "The chance of your child having brown eyes is 75%, green is 18.75%, blue is 6.25%"
+//         // );
+//         break;
+//     case "GreenBrown":
+//     case "BrownGreen":
+//     case "browngreen":
+//     case "greenbrown":
+//         console.log(
+//             "The chance of your child having brown eyes is 50%, green is 37.5%, blue is 12.5%"
+//         );
+//         break;
+//     case "BlueBrown":
+//     case "BrownBlue":
+//     case "brownblue":
+//     case "bluebrown":
+//         console.log(
+//             "The chance of your child having brown eyes is 50%, blue is 50%, green is 0%"
+//         );
+//         break;
+//     case "GreenGreen":
+//     case "greengreen":
+//         console.log(
+//             "The chance of your child having green eyes is 75%, blue is 25%, brown is <1%"
+//         );
+//         break;
+//     case "GreenBlue":
+//     case "BlueGreen":
+//     case "bluegreen":
+//     case "greenblue":
+//         console.log(
+//             "The chance of your child having green eyes is 50%, blue is 50%, brown is 0%"
+//         );
+//         break;
+//     case "BlueBlue":
+//     case "blueblue":
+//         console.log(
+//             "The chance of your child having blue eyes is 99%, green is 1%, brown is 0%"
+//         );
+//         break;
+//     default:
+//         console.log("Invalid Eye Color");
+// }
+
+console.log(
+    `The chance of your child having brown eyes is ${browPercent}, green is ${greenPercent}, blue is ${bluePercent}`
+);
+
+// const parent1 = "brown";
+// const parent2 = "brown";
+// switch (true) {
+//     case parent1 === "brown" && parent2 === "brown":
+//         console.log(
+//             `Your eye colours are ${parent1} and ${parent2}, the chances of your child having blue eyes is 6.25%, brown is 75% and green is 18.75%`
+//         );
+//         break;
+//     case (parent1 === "green" && parent2 === "brown") ||
+//         (parent2 === "green" && parent1 === "brown"):
+//         console.log(
+//             `Your eye colours are ${parent1} and ${parent2}, the chances of your child having blue eyes is 12.5%, brown is 50% and green is 37.5%`
+//         );
+//         break;
+//     case (parent1 === "blue" && parent2 === "brown") ||
+//         (parent2 === "blue" && parent1 === "brown"):
+//         console.log(
+//             `Your eye colours are ${parent1} and ${parent2}, the chances of your child having blue eyes is 50%, brown is 50% and green is 0%`
+//         );
+//         break;
+//     case parent1 === "green" && parent2 === "green":
+//         console.log(
+//             `Your eye colours are ${parent1} and ${parent2}, the chances of your child having blue eyes is 25%, brown is <1% and green is 75%`
+//         );
+//         break;
+//     case (parent1 === "green" && parent2 === "blue") ||
+//         (parent2 === "green" && parent1 === "blue"):
+//         console.log(
+//             `Your eye colours are ${parent1} and ${parent2}, the chances of your child having blue eyes is 50%, brown is 0% and green is 50%`
+//         );
+//         break;
+//     case parent1 === "blue" && parent2 === "blue":
+//         console.log(
+//             `Your eye colours are ${parent1} and ${parent2}, the chances of your child having blue eyes is 99%, brown is 0% and green is 1%`
+//         );
+//         break;
+//     default:
+//         console.log("invalid inputs");
+// }
+
+let parent1 = "green";
+let parent2 = "brown";
+let brown;
+let blue;
+let green;
+
+let pool = 0;
+switch (parent1) {
+    case "brown":
+        pool += 1;
+        break;
+    case "green":
+        pool += 2;
+        break;
+    case "blue":
+        pool += 4;
+        break;
+}
+switch (parent2) {
+    case "brown":
+        pool += 1;
+        break;
+    case "green":
+        pool += 2;
+        break;
+    case "blue":
+        pool += 4;
+        break;
+}
+switch (pool) {
+    // Two Browns
+    case 2:
+        brown = "75";
+        blue = "6.25";
+        green = "18.75";
+        break;
+    // Brown and Green
+    case 3:
+        brown = "50";
+        blue = "12.5";
+        green = "37.5";
+        break;
+    // Green and Green
+    case 4:
+        brown = "<1";
+        blue = "25";
+        green = "75";
+        break;
+    // Brown and Blue
+    case 5:
+        brown = "50";
+        blue = "50";
+        green = "0";
+        break;
+    // Blue and Green
+    case 6:
+        brown = "0";
+        blue = "50";
+        green = "50";
+        break;
+    // Blue and Blue
+    case 8:
+        brown = "0";
+        blue = "99";
+        green = "<1";
+        break;
+}
+console.log(
+    `Your eye colors are ${parent1} and ${parent2}, the chancers of your child having blue eyes is ${blue}%, brown is ${brown}%, and green is ${green}%`
+);
