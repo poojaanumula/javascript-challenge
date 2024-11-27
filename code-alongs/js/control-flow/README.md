769a66985152a871064f3838c7e251b9e9db798a
Martyna Król <martyna.krool@gmail.com>
2024-11-27 11:10:00 +1100

    switch

--- code-alongs/js/control-flow/README.md
+++ code-alongs/js/control-flow/README.md
@@ -34,3 +34,27 @@ Console log the user's input depending on the data type
 1. If the input is a number, it should console log that number but squared
 1. If the input is a string, it should console log that input
 1. If the input is anything else, the console should say "invalid input"
+
+## Challenge: Tell me the day!
+
+### MVP:
+
+Create a switch block that will log a string for each day of the week.
+
+1. Create a variable called `day`
+2. The switch will be given a number and return the matching days of the week
+3. e.g. if `day = 2`, the switch should print `Today is Tuesday` in the console.
+4. Your switch block should also handle numbers out of range
+
+## Challenge: Eye Colour
+
+### MVP:
+
+Tell the user what eye colour their child would have.
+
+1. Create two variables `parent1` and `parent2`
+2. Given the eye colour of each parent, it should console log the probability
+   of their child having different eye colours, for example:
+   `Your eye colours are brown and blue, the chances of your child having blue eyes is --%, brown is --% and green is --%`
+3. Use the following image as a reference
+   ![alt text](image.png)
--- code-alongs/js/control-flow/script.js
+++ code-alongs/js/control-flow/script.js
@@ -67,3 +67,116 @@ if (score >= 90) {
 //     console.log("C");
 // }
 console.log("run the rest of the code");
+
+// EVEN OR ODD
+let n = 4;
+
+if (n % 2 === 0) {
+    console.log("n is even");
+} else {
+    console.log("n is odd");
+}
+
+if (!(n % 2)) {
+    console.log("n is even");
+} else {
+    console.log("n is odd");
+}
+
+if (n % 2) {
+    console.log("n is odd");
+} else {
+    console.log("n is even");
+}
+// COMPARING NUMBERS
+let x = 10;
+let y = 20;
+if (x > y) {
+    console.log("x is greater than y");
+} else if (x < y) {
+    console.log("x is smaller than y");
+} else {
+    console.log("x is equal to y");
+}
+
+// USER INPUT
+const userInput = true;
+
+if (typeof userInput === "number") {
+    const numSquared = userInput * userInput;
+    console.log(numSquared);
+} else if (typeof userInput === "string") {
+    console.log(userInput);
+} else {
+    console.log("invalid input!");
+}
+
+// alternative to if else
+// switch statement
+
+// calculator
+
+const num1 = 23;
+const num2 = 2;
+let op = "-";
+
+if (op === "+") {
+    console.log(num1 + num2);
+} else if (op === "-") {
+    console.log(num1 - num2);
+} else if (op === "*") {
+    console.log(num1 * num2);
+} else if (op === "/") {
+    console.log(num1 / num2);
+} else {
+    console.log("error");
+}
+console.log("Start of switch");
+switch (op) {
+    case "+":
+        console.log(num1 + num2);
+        break;
+    case "-":
+        console.log(num1 - num2);
+        break;
+    case "*":
+        console.log(num1 * num2);
+        break;
+    case "/":
+        console.log(num1 / num2);
+        break;
+    default:
+        console.log("error");
+}
+
+const input = "exit";
+
+switch (input) {
+    case "e":
+    case "E":
+    case "exit":
+        console.log("Exit the app");
+        break;
+    case "c":
+        console.log("Continue running the app");
+        break;
+    default:
+        console.log("Invalid command");
+}
+
+if (input === "e" || input === "E" || input === "exit") {
+    console.log("Exit the app");
+}
+
+let num = 1;
+// if number is more than 0 but less than 2
+// num > 0 && num < 2
+
+if (num > 0 && num < 2) {
+    console.log("correct number");
+}
+
+switch (true) {
+    case num > 0 && num < 2:
+        console.log("correct number from switch");
+}
